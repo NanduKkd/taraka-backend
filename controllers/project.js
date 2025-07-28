614b16fea24dd212137895ddff6b5c2a04e9b56e
nandukkd <nandukkd7164@gmail.com>
2025-07-28 06:45:15 +0530

    feat: implement createProject API

--- controllers/project.js
+++ controllers/project.js
@@ -1,9 +1,22 @@
 
 const AppError = require('../utils/AppError');
+const Project = require('../models/Project');
+const Session = require('../models/Session');
 
-const createProject = (req, res) => {
-  // TODO: Implement createProject
-  throw new AppError('Not Implemented').setCode(AppError.CODES.NOT_IMPLEMENTED);
+const createProject = async (req, res) => {
+  const { machineId, os, path, name, description } = req.body;
+
+  let project = await Project.findOne({ machineId, os, path });
+
+  if (project) {
+    const sessions = await Session.find({ projectId: project._id });
+    return res.helper.success({ project, sessions }, 'Project already exists');
+  }
+
+  project = await Project.create({ machineId, os, path, name, description });
+  const session = await Session.create({ projectId: project._id });
+  
+  res.helper.success({ project, sessions: [session] }, 'Project created successfully', 201);
 };
 
 const getProjects = (req, res) => {
