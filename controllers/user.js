416a103708ae5860e71600497d1e76190a7cb126
nandukkd <nandukkd7164@gmail.com>
2025-07-28 05:48:45 +0530

    feat: add bcrypt dependency

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,25 @@
 
-const login = (req, res) => {
-  // TODO: Implement login functionality
-  res.status(501).send({ message: 'Not Implemented' });
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    res.status(200).json({ message: 'Login successful', user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const logout = (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -13,6 +14,13 @@ const UserSchema = new mongoose.Schema({
   name: {
     type: String,
   },
+});
+
+UserSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
